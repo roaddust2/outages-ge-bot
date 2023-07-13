f73e56d0c187c5b878972a9ac9bb1099b4f95afb
roaddust2 <roaddust2@yahoo.com>
2023-07-13 16:00:49 +0400

    Create app

---
+++ app/main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+
+load_dotenv()
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+API_TOKEN = os.getenv('API_TOKEN')
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
