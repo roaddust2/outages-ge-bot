41dc6c3596a8f46e7da2fb0b3d840a50dcd8a6fc
roaddust2 <roaddust2@yahoo.com>
2023-07-24 09:36:54 +0400

    Add migration to Outages

---
+++ migrations/versions/c5939b9c4bda_add_unique_constraint_and_repr.py
@@ -0,0 +1,24 @@
+"""Add unique constraint and repr
+
+Revision ID: c5939b9c4bda
+Revises: 7f755b058fde
+Create Date: 2023-07-23 16:11:56.455708
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c5939b9c4bda'
+down_revision = '7f755b058fde'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
