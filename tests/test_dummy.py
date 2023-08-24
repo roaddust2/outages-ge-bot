7d79ee320b6e73aa5ceca6c2484d177f91ae5d60
roaddust2 <roaddust2@yahoo.com>
2023-08-24 10:58:54 +0400

    Fix linter errors

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,12 +1,13 @@
 from app.handlers.jobs import format_info
 
+
 # TODO: Add tests
 
 def test_dummy():
 
     info = "Outage on Street"
     street = "Street"
-    
+
     result = "Outage on <b>Street</b>"
 
     assert format_info(info, street) == result
