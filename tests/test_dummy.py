dd7c67e8b7ac2b753c3ff1f127c5b266d5a84543
roaddust2 <roaddust2@yahoo.com>
2023-08-24 10:57:26 +0400

    Complete dummy test for coverage

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,4 +1,14 @@
+from app.handlers.jobs import format_info
+
 # TODO: Add tests
 
 def test_dummy():
-    assert True
+
+    info = "Outage on Street"
+    street = "Street"
+    
+    result = "Outage on <b>Street</b>"
+
+    assert format_info(info, street) == result
+    assert format_info(info, "Not existing street") != result
+    assert format_info(info, "Not existing street") == info
