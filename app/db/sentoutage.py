e4c453711bed68e540ae3d3be7dc105462d6ccd9
roaddust2 <roaddust2@yahoo.com>
2023-09-08 11:35:09 +0400

    Add created_at field to SentOutage, job fix

--- app/db/sentoutage.py
+++ app/db/sentoutage.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import JSON
-from sqlalchemy import ForeignKey, Integer
+from sqlalchemy import func, ForeignKey, Integer, DateTime
 from app.db.base import Base
+from datetime import datetime
 
 
 class SentOutage(Base):
@@ -12,3 +13,4 @@ class SentOutage(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
     outage: Mapped[dict] = mapped_column(JSON, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=True, default=func.now())
